e554bfed69f3c613a0428c272f47d1678395df44
dio067 <yousefnas06@gmail.com>
2026-03-21 03:37:44 +0400

    feat: implement insertion version 2 with boolean flag

--- src/sorting/implementation/insertion.cpp
+++ src/sorting/implementation/insertion.cpp
@@ -17,3 +17,27 @@ void insertion_V1(int arr[], int n)
         arr[j + 1] = key;
     }
 }
+
+void insertion_V2(int arr[], int n)
+{
+    for (int i{1}; i < n; i++)
+    {
+        bool swapped = false;
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key)
+        {
+            arr[j + 1];
+            j--;
+            swapped = true;
+        }
+
+        arr[j + 1] = key;
+
+        if (!swapped)
+        {
+            break;
+        }
+    }
+}
