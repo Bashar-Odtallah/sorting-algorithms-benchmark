35d06028da2984e94c11f825846428aedbe38c93
dio067 <yousefnas06@gmail.com>
2026-03-31 22:46:11 +0400

    feat: add insertion function for small arrays

--- src/sorting/implementation/quick.cpp
+++ src/sorting/implementation/quick.cpp
@@ -48,3 +48,18 @@ void quick_V1(int arr[], int n)
 {
     quickSortV1(arr, 0, n - 1);
 }
+
+void insertionForQuick(int arr[], int first, int last)
+{
+    for (int i = first + 1; i <= last; i++)
+    {
+        int key = arr[i];
+        int j = i - 1;
+        while (j >= first && arr[j] > key)
+        {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+}
