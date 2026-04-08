36f68be2994dd0c3cdad5eb0c4a6cd87bd7d357a
Bashar-Odtallah <2024007070@aurak.ac.ae>
2026-04-08 21:42:39 +0400

    Update QuickSort V1 and add improved randomized V2 with insertion sort

--- src/sorting/implementation/quick.cpp
+++ src/sorting/implementation/quick.cpp
@@ -1,39 +1,46 @@
 #include "../header/quick.hpp"
+#include <cstdlib>  // for rand()
+#include <ctime>    // for srand()
+#include <algorithm>
 
+// ------------------------- Partition (Hoare) -------------------------
 int partition(int T[], int first, int last)
 {
     int pivot = T[first];
     int bottom = first;
     int top = last;
-    int loop = 1;
     int cutPoint;
 
-    while (loop)
+    while (true)
     {
-
-        while (T[top] > pivot)
-            top--;
-
-        while (T[bottom] < pivot)
-            bottom++;
+        while (T[top] > pivot) top--;
+        while (T[bottom] < pivot) bottom++;
 
         if (bottom < top)
         {
-
             int temp = T[bottom];
             T[bottom] = T[top];
             T[top] = temp;
         }
         else
         {
-            loop = 0;
             cutPoint = top;
+            break;
         }
     }
 
     return cutPoint;
 }
 
+// ------------------------- Randomized Partition -------------------------
+int randomizedPartition(int T[], int first, int last)
+{
+    int pivotIndex = first + rand() % (last - first + 1);
+    std::swap(T[first], T[pivotIndex]);
+    return partition(T, first, last);
+}
+
+// ------------------------- QuickSort V1 (original) -------------------------
 void quickSortV1(int T[], int first, int last)
 {
     if (first < last)
@@ -49,6 +56,7 @@ void quick_V1(int arr[], int n)
     quickSortV1(arr, 0, n - 1);
 }
 
+// ------------------------- Insertion Sort for Small Arrays -------------------------
 void insertionForQuick(int arr[], int first, int last)
 {
     for (int i = first + 1; i <= last; i++)
@@ -64,22 +72,26 @@ void insertionForQuick(int arr[], int first, int last)
     }
 }
 
+// ------------------------- QuickSort V2 (Improved) -------------------------
 void quickSortV2(int T[], int first, int last)
 {
     if (first < last)
     {
+        // Use insertion sort for small subarrays
         if (last - first <= 10)
         {
             insertionForQuick(T, first, last);
             return;
         }
-        int cut = partition(T, first, last);
+
+        int cut = randomizedPartition(T, first, last); // Randomized pivot
         quickSortV2(T, first, cut);
         quickSortV2(T, cut + 1, last);
     }
-} // comment 2.0
+}
 
 void quick_V2(int arr[], int n)
 {
+    srand(static_cast<unsigned>(time(nullptr))); // Seed random once
     quickSortV2(arr, 0, n - 1);
 }
